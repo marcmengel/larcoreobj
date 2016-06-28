ee00b3b996c00a90dda48d190ed31cf94e9ef432
Lynn Garren <garren@fnal.gov>
2016-06-28 12:20:26 -0500

    remove unnecessary template instantiations

--- larcoreobj/SummaryData/classes.h
+++ larcoreobj/SummaryData/classes.h
@@ -2,9 +2,3 @@
 
 #include "larcoreobj/SummaryData/RunData.h"
 #include "larcoreobj/SummaryData/POTSummary.h"
-
-template class std::vector<sumdata::RunData>;
-template class art::Wrapper<sumdata::RunData>;
-template class std::vector<sumdata::POTSummary>;
-template class art::Wrapper<sumdata::POTSummary>;
-
