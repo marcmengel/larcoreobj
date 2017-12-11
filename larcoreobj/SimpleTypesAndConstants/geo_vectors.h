4a7dc838ee3fe960960a075e839b826e267d8a31
Gianluca Petrillo <petrillo@fnal.gov>
2017-12-11 15:15:12 -0600

    Introduced more generic vector types.

--- larcoreobj/SimpleTypesAndConstants/geo_vectors.h
+++ larcoreobj/SimpleTypesAndConstants/geo_vectors.h
@@ -23,7 +23,50 @@
 
 namespace geo {
   
+  ///@{
+  ///@name Generic vector types.
+  
+  /// Type of 3D displacement vector.
+  /// @tparam T data type for coordinate representation
+  /// @tparam C coordinate system tag (default: global coordinates)
+  template <typename T, typename C = ROOT::Math::GlobalCoordinateSystemTag>
+  using GenVector3DBase_t
+    = ROOT::Math::DisplacementVector3D<ROOT::Math::Cartesian3D<T>, C>;
+  
+  /// Type of 3D point.
+  /// @tparam T data type for coordinate representation
+  /// @tparam C coordinate system tag (default: global coordinates)
+  template <typename T, typename C = ROOT::Math::GlobalCoordinateSystemTag>
+  using GenPoint3DBase_t
+    = ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<T>, C>;
+  
+  /// Type of 3D displacement vector with representation in double precision.
+  /// @tparam C coordinate system tag
+  template <typename C>
+  using Vector3DBase_t = GenVector3DBase_t<double, C>;
+  
+  /// Type of 3D point with representation in double precision.
+  template <typename C>
+  /// @tparam C coordinate system tag
+  using Point3DBase_t = GenPoint3DBase_t<double, C>;
+  
+  /// @}
+  
+  /// @{
   /**
+   * @name Vector types for the standard LArSoft geometry.
+   * 
+   * LArSoft geometry provides two main types of vectors in 3D space:
+   * 
+   * 1. `geo::Point_t` to describe an absolute position in global coordinates
+   * 2. `geo::Vector_t` to describe a displacement or direction (or momentum!)
+   * 
+   * Both vectors are supposed to represent:
+   * 
+   * * centimeters in double precision when used on the real geometry space
+   * * in the global coordinate system, which is represented by the tag
+   *   `geo::GlobalCoords`.
+   * 
    * These types constitute the basic objects the geometry works with.
    * 
    * All interfaces should support them.
@@ -34,8 +77,36 @@ namespace geo {
    *       https://cdcvs.fnal.gov/redmine/projects/larsoft/issues .
    *       Even if there are plenty.
    * 
+   * The same type of vectors, but in a different coordinate system
+   * representation, can be obtained by using `geo::Point3DBase_t` template:
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+   * using LocalPoint_t = geo::Point3DBase_t<LocalCoordinateTag>;
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   * (`geo::Vector3DBase_t` is also available).
+   * If a single precision vector is desired, the most general
+   * `geo::GenPoint3DBase_t` and `geo::GenVector3DBase_t` are also available:
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~{.cpp}
+   * using PointF_t = geo::GenPoint3DBase_t<float>;
+   * using VectorF_t = geo::GenVector3DBase_t<float>;
+   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   * 
    */
-  /// @{
+  
+  /// Type used for coordinates and distances. They are measured in centimeters.
+  using Length_t = double;
+  
+  /**
+   * @brief Tag for vectors in the global coordinate system.
+   * 
+   * A vector tagged as "global" is expected to be represented in the global
+   * (or "world") coordinate system.
+   * 
+   * That system is the one the detector geometry is described in, and it is
+   * defined by the GDML detector description.
+   * The linear coordinates are described in _centimeters_.
+   * 
+   */
+  using GlobalCoords = ROOT::Math::GlobalCoordinateSystemTag;
   
   /**
    * @brief Type for representation of momenta in 3D space.
@@ -46,11 +117,10 @@ namespace geo {
    * Their modulus can also be scaled.
    * 
    */
+  // the actual definition commented out is not understood by GenReflex
+//  using Vector_t = Vector3DBase_t<GlobalCoords>;
   using Vector_t = ROOT::Math::DisplacementVector3D
     <ROOT::Math::Cartesian3D<double>, ROOT::Math::GlobalCoordinateSystemTag>;
-  
-  /// Type used for coordinates and distances. They are measured in centimeters.
-  using Length_t = typename Vector_t::Scalar;
   
   /**
    * @brief Type for representation of position in physical 3D space.
@@ -67,21 +137,32 @@ namespace geo {
    * of vector and in particular `geo::Point_t`.
    * 
    */
-  using Point_t = ROOT::Math::PositionVector3D
-    <ROOT::Math::Cartesian3D<double>, ROOT::Math::GlobalCoordinateSystemTag>;
+  // the actual definition commented out is not understood by GenReflex
+//  using Point_t = Point3DBase_t<GlobalCoords>;
+   using Point_t = ROOT::Math::PositionVector3D
+     <ROOT::Math::Cartesian3D<double>, ROOT::Math::GlobalCoordinateSystemTag>;
+  
   
   /**
-   * @brief Tag for vectors in the global coordinate system.
+   * @brief Type for representation of momenta in 3D space.
+   * @tparam CoordSystemTag the coordinate system tag for this vector
    * 
-   * A vector tagged as "global" is expected to be represented in the global
-   * (or "world") coordinate system.
-   * 
-   * That system is the one the detector geometry is described in, and it is
-   * defined by the GDML detector description.
-   * The linear coordinates are described in _centimeters_.
-   * 
+   * This vector type is equivalent to `geo::Vector_t` but it's tagged as from a
+   * different coordinate system.
    */
-  using GlobalCoords = ROOT::Math::GlobalCoordinateSystemTag;
+  template <typename CoordSystemTag>
+  using VectorIn_t = Vector3DBase_t<CoordSystemTag>;
+  
+  /**
+   * @brief Type for representation of positions in 3D space.
+   * @tparam CoordSystemTag the coordinate system tag for this point
+   * 
+   * This point type is equivalent to `geo::Point_t` but it's tagged as from a
+   * different coordinate system.
+   */
+  template <typename CoordSystemTag>
+  using PointIn_t = Point3DBase_t<CoordSystemTag>;
+  
   
   /// Type for representation of space rotations.
   using Rotation_t = ROOT::Math::Rotation3D;
@@ -103,6 +184,8 @@ namespace geo {
   /// Returns a origin position with a point of the specified type.
   template <typename Point = Point_t>
   constexpr Point origin() { return { 0.0, 0.0, 0.0 }; }
+  
+  /// @}
   
   
   //--------------------------------------------------------------------------
